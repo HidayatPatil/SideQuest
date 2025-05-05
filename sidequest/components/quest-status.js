35f04a10cc4cad1bdda1ce07546f6c78da07365e
Hidayat Patil <hpatil2@my.bcit.ca>
2025-05-05 12:25:52 -0700

    fixed structure and added sections

--- sidequest/components/quest-status.js
+++ sidequest/components/quest-status.js
@@ -2,6 +2,9 @@ import styles from "@/components/quest-status.module.css";
 
 export default function QuestStatus() {
 	return (
-	<div className={styles.cardStatus}>Ending Soon</div>
-    )
+		<div className={styles.cardStatus}>
+			<img className={styles.endingSoon} src="/Icons/endingSoon.svg" />
+			Ending Soon
+		</div>
+	);
 }
