c21acc0d2abcb09d4a91908679e17d50e7936ded
Hidayat Patil <hpatil2@my.bcit.ca>
2025-05-05 02:46:40 -0700

    update quest card and added navigation

---
+++ sidequest/components/quest-status.js
@@ -0,0 +1,7 @@
+import styles from "@/components/quest-status.module.css";
+
+export default function QuestStatus() {
+	return (
+	<div className={styles.cardStatus}>Ending Soon</div>
+    )
+}
--- sidequest/pages/chat.js
+++ sidequest/pages/chat.js
@@ -1,9 +1,11 @@
+import NavBar from "@/components/nav-bar";
 
 export default function Chat() {
 	return (
 		<main>
 			<h1>Welcome to Quest Chats</h1>
 			<p>Connect with fellow adventures for your journey ahead!</p>
+			<NavBar/>
 		</main>
 	);
 }
