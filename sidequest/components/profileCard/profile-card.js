a7fd6c6a88e9f538a0ad50b58ae3b0a918caa14a
Leana <leanale003@gmail.com>
2025-05-12 15:13:42 -0700

    add ProfileCard component css and, add ThumbsUp icon

--- sidequest/components/profileCard/profile-card.js
+++ sidequest/components/profileCard/profile-card.js
@@ -2,31 +2,46 @@ import styles from "@/components/profileCard/profile-card.module.css";
 import Link from "next/link";
 
 export default function ProfileCard() {
-	return (
-		<Link href="/profile" passHref>
-			<div className={styles.profileContainer}>
-				<div className={styles.profileMain}>
-				<img className={styles.profileImage} src="\profile_Image\Maya.jpg"/>
-					<div className={styles.profileContent}>
-						<div className={styles.profileData}>
-							<div className={styles.userIdentification}>
-								<div className={styles.userName}>Maya</div>
-								<div className={styles.userID}>ID#149385</div>
-							</div>
-							<div className={styles.userRank}>
-								<div className={styles.userRole}>Novice Quester</div>
-								<div className={styles.userRating}>
-									25
-									<img />
-								</div>
-							</div>
-						</div>
-						<div className={styles.leveling}>
-							<div className={styles.levelBar}></div>5 more upvotes to level up
-						</div>
-					</div>
-				</div>
-			</div>
-		</Link>
-	);
+    return (
+        <Link
+            href="/profile"
+            passHref
+        >
+            <div className={styles.profileContainer}>
+                <div className={styles.profileMain}>
+                    <img
+                        className={styles.profileImage}
+                        src="\profile_Image\Maya.jpg"
+                    />
+                    <div className={styles.profileContent}>
+                        <div className={styles.profileData}>
+                            <div className={styles.userIdentification}>
+                                <div className={styles.userName}>Maya</div>
+                                <div className={styles.userID}>ID#149385</div>
+                            </div>
+                            <div className={styles.userRank}>
+                                <div className={styles.userRole}>
+                                    Novice Quester
+                                </div>
+                                <div className={styles.userRating}>
+                                    25
+                                    <img
+                                        className={styles.userIcon}
+                                        src="/Icons/ThumbsUp.svg"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.leveling}>
+                            <div className={styles.levelBar}>
+                                <div className={styles.levelProgress}></div>
+                            </div>
+                            <div className={styles.levelCount}></div>5 more
+                            upvotes to level up
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
 }
