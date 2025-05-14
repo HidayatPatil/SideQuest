aeb91fd8caa06a92520f0555f853e37635e1c533
Leana <leanale003@gmail.com>
2025-05-14 14:05:07 -0700

    profile css for stats card and centered in page

--- sidequest/pages/profile.js
+++ sidequest/pages/profile.js
@@ -15,46 +15,49 @@ export default function Profile() {
                     <h3>STATS</h3>
                 </div>
                 <div className="profileStats">
-                    <div>
-                        Bonjour! I am new to Vancouver and wanting to explore
-                        the amazing nature here.
-                    </div>
-                    <hr className="divider" />
                     <div className="profileStatsContainer">
-                        <h4>Pronouns</h4>
-                        <div className="profileTag">She/Her</div>
-                    </div>
-                    <div className="profileStatsContainer">
-                        <h4>Personality</h4>
-                        <div>
-                            Passionate about learning, exploring new places, and
-                            meeting new people! Big nerd and a chill explorer,
-                            I’m probably deep in my studies or halfway up a
-                            trail.
+                        <div className="profileStatsSection bio">
+                            Bonjour! I am new to Vancouver and wanting to
+                            explore the amazing nature here.
                         </div>
-                    </div>
-                    <div className="profileStatsContainer">
-                        <h4>Cultural Background</h4>
-                        <div>
-                            I grew up in the countryside and am currently based
-                            in Vancouver for school. As a first-generation
-                            Canadian I find community greatly important.
+                        <hr className="divider" />
+                        <div className="profileStatsSection">
+                            <h4>Pronouns</h4>
+                            <div className="profileTag">She/Her</div>
                         </div>
-                    </div>
-                    <div className="profileStatsContainer">
-                        <h4>Occupation</h4>
-                        <div>Medical Student</div>
-                    </div>
-                    <div className="profileStatsContainer">
-                        <h4>Languages</h4>
-                        <div className="profileTags">
-                            <div className="profileTag">English</div>
-                            <div className="profileTag">French</div>
+                        <div className="profileStatsSection">
+                            <h4>Personality</h4>
+                            <div>
+                                Passionate about learning, exploring new places,
+                                and meeting new people! Big nerd and a chill
+                                explorer, I’m probably deep in my studies or
+                                halfway up a trail.
+                            </div>
                         </div>
-                    </div>
-                    <div className="profileStatsContainer">
-                        <h4>Accessibility Needs</h4>
-                        <div className="profileTag">Hearing Impaired</div>
+                        <div className="profileStatsSection">
+                            <h4>Cultural Background</h4>
+                            <div>
+                                I grew up in the countryside and am currently
+                                based in Vancouver for school. As a
+                                first-generation Canadian I find community
+                                greatly important.
+                            </div>
+                        </div>
+                        <div className="profileStatsSection">
+                            <h4>Occupation</h4>
+                            <div>Medical Student</div>
+                        </div>
+                        <div className="profileStatsSection">
+                            <h4>Languages</h4>
+                            <div className="profileTags">
+                                <div className="profileTag">English</div>
+                                <div className="profileTag">French</div>
+                            </div>
+                        </div>
+                        <div className="profileStatsSection">
+                            <h4>Accessibility Needs</h4>
+                            <div className="profileTag">Hearing Impaired</div>
+                        </div>
                     </div>
                 </div>
 
