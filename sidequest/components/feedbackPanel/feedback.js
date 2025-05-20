30a74f22e96fc90ad0704c987af3fb13d129e3cc
Leana <leanale003@gmail.com>
2025-05-19 23:25:58 -0700

    added upvote/downvote diff states and snackbar pop up interaction!

--- sidequest/components/feedbackPanel/feedback.js
+++ sidequest/components/feedbackPanel/feedback.js
@@ -1,74 +1,106 @@
 import styles from "@/components/feedbackPanel/feedback.module.css";
 import Button from "../button/button";
+import { useState } from "react";
+
+const members = [
+    { name: "Jordan", id: "149385", img: "/profileImage/JD.png" },
+    { name: "Maya", id: "109259", img: "/profileImage/Maya.jpg" },
+    { name: "Gilbert", id: "165510", img: "/profileImage/Gilbert.png" },
+    { name: "Dylany", id: "149840", img: "/profileImage/Dylany.png" },
+];
 
 export default function FeedBack({ onClose }) {
-	return (
-		<div className={styles.panelBody}>
-			{/* <div
-				onClick={onClose}
-				style={{ position: "absolute", right: 20, top: 20, cursor: "pointer" }}
-			>
-				<img src="/Icons/X.svg" alt="Close" />
-			</div> */}
-			<div className={styles.panelHandle} onClick={onClose}></div>
-			<p className={styles.panelTitle}>
-				Send an upvote to a quester from your party
-			</p>
-			<div className={styles.memberCard}>
-				<img className={styles.memberImage} src="\profileImage\JD.png" />
-				<div className={styles.memberDetails}>
-					<h4>Jordan</h4>
-					<p>ID#149385</p>
-				</div>
-				<div className={styles.upvote}>
-					<img src="\Icons\feedbackThumbsup.svg" />
-				</div>
-				<div className={styles.downvote}>
-					<img src="\Icons\feedbackWarning.svg" />
-				</div>
-			</div>
-			<div className={styles.memberCard}>
-				<img className={styles.memberImage} src="\profileImage\Maya.jpg" />
-				<div className={styles.memberDetails}>
-					<h4>Maya</h4>
-					<p>ID#109259</p>
-				</div>
-				<div className={styles.upvote}>
-					<img src="\Icons\feedbackThumbsup.svg" />
-				</div>
-				<div className={styles.downvote}>
-					<img src="\Icons\feedbackWarning.svg" />
-				</div>
-			</div>
-			<div className={styles.memberCard}>
-				<img className={styles.memberImage} src="\profileImage\Gilbert.png" />
-				<div className={styles.memberDetails}>
-					<h4>Gilbert</h4>
-					<p>ID#165510</p>
-				</div>
-				<div className={styles.upvote}>
-					<img src="\Icons\feedbackThumbsup.svg" />
-				</div>
-				<div className={styles.downvote}>
-					<img src="\Icons\feedbackWarning.svg" />
-				</div>
-			</div>
-			<div className={styles.memberCard}>
-				<img className={styles.memberImage} src="\profileImage\Dylany.png" />
-				<div className={styles.memberDetails}>
-					<h4>Dylany</h4>
-					<p>ID#149840</p>
-				</div>
-				<div className={styles.upvote}>
-					<img src="\Icons\feedbackThumbsup.svg" />
-				</div>
-				<div className={styles.downvote}>
-					<img src="\Icons\feedbackWarning.svg" />
-				</div>
-			</div>
-			<div className={styles.submitButton}>
-				<Button text="Submit" variant="primary" onClick={onClose} />
-			</div>
-		</div>
-	);
+    const [feedbackState, setFeedbackState] = useState(
+        members.map(() => ({ upvoted: false, downvoted: false }))
+    );
+
+    const toggleUpvote = (index) => {
+        setFeedbackState((prev) =>
+            prev.map((f, i) =>
+                i === index ? { ...f, upvoted: !f.upvoted } : f
+            )
+        );
+    };
+
+    const toggleDownvote = (index) => {
+        setFeedbackState((prev) =>
+            prev.map((f, i) =>
+                i === index ? { ...f, downvoted: !f.downvoted } : f
+            )
+        );
+    };
+
+    const handleSubmit = () => {
+        onClose(); // Parent handles closing + showing snackbar
+    };
+
+    return (
+        <div className={styles.panelBody}>
+            <div
+                className={styles.panelHandle}
+                onClick={onClose}
+            ></div>
+            <p className={styles.panelTitle}>
+                Send an upvote to a quester from your party
+            </p>
+
+            {members.map((member, index) => {
+                const { upvoted, downvoted } = feedbackState[index];
+                return (
+                    <div
+                        className={styles.memberCard}
+                        key={member.id}
+                    >
+                        <img
+                            className={styles.memberImage}
+                            src={member.img}
+                            alt={member.name}
+                        />
+                        <div className={styles.memberDetails}>
+                            <h4>{member.name}</h4>
+                            <p>ID#{member.id}</p>
+                        </div>
+                        <div
+                            className={`${styles.upvote} ${
+                                upvoted ? styles.upvoteActive : ""
+                            }`}
+                            onClick={() => toggleUpvote(index)}
+                        >
+                            <img
+                                src={
+                                    upvoted
+                                        ? "/Icons/feedbackThumbsupWhite.svg"
+                                        : "/Icons/feedbackThumbsup.svg"
+                                }
+                                alt="Upvote"
+                            />
+                        </div>
+                        <div
+                            className={`${styles.downvote} ${
+                                downvoted ? styles.downvoteActive : ""
+                            }`}
+                            onClick={() => toggleDownvote(index)}
+                        >
+                            <img
+                                src={
+                                    downvoted
+                                        ? "/Icons/feedbackWarningWhite.png"
+                                        : "/Icons/feedbackWarning.svg"
+                                }
+                                alt="Downvote"
+                            />
+                        </div>
+                    </div>
+                );
+            })}
+
+            <div className={styles.submitButton}>
+                <Button
+                    text="Submit"
+                    variant="primary"
+                    onClick={handleSubmit}
+                />
+            </div>
+        </div>
+    );
 }
