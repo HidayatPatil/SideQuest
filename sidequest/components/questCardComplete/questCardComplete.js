8a6432bf71714d44ef15dc166a1e1b305ecb6cc8
Lucie Zhong <luciez@Lucies-MacBook-Air.local>
2025-05-20 18:30:41 -0700

    minor change to feedback button

--- sidequest/components/questCardComplete/questCardComplete.js
+++ sidequest/components/questCardComplete/questCardComplete.js
@@ -7,58 +7,55 @@ import Link from "next/link";
 import FeedBack from "../feedbackPanel/feedback";
 
 export default function QuestCardComplete({
-	id,
-	img,
-	title,
-	tags,
-	memberCount,
-	date,
-	onFeedbackClick, // ✅ Accept the prop
+    id,
+    img,
+    title,
+    tags,
+    memberCount,
+    date,
+    onFeedbackClick, // ✅ Accept the prop
 }) {
-	return (
-		<CardVisual>
-			<Link href={`/questDetail/${id}`} passHref>
-				<div className={styles.mainBody}>
-					<img className={styles.questImage} src={img} alt={title} />
-					<div className={styles.mainContent}>
-						<div className={styles.cardTitle}>
-							<h2 className={styles.titleText}>{title}</h2>
-							<img
-								className={styles.cardIcons}
-								src="\Icons\questCompleteWarning.svg"
-								alt="Save for later"
-							/>
-						</div>
-						<div className={styles.cardTags}>
-							{tags.map((tag, index) => (
-								<QuestTags key={index} tagName={tag} />
-							))}
-						</div>
-						<div className={styles.cardDetails}>
-							<div className={styles.questDate}>
-								<img className={styles.cardIcons} src="/Icons/questDate.svg" />
-								{date}
-							</div>
-							<div className={styles.memberCount}>
-								<img
-									className={styles.cardIcons}
-									src="/Icons/questMembers.svg"
-								/>
-								{memberCount}
-							</div>
-						</div>
-					</div>
-				</div>
-			</Link>
-			<div className={styles.footerBody}>
-				<div className={styles.questFeedback}>
-					<Button
-						text="Give Feedback"
-						variant="outline"
-						onClick={onFeedbackClick} // ✅ call the handler passed from page
-					/>
-				</div>
-			</div>
-		</CardVisual>
-	);
+    return (
+        <CardVisual>
+            <Link href={`/questDetail/${id}`} passHref>
+                <div className={styles.mainBody}>
+                    <img className={styles.questImage} src={img} alt={title} />
+                    <div className={styles.mainContent}>
+                        <div className={styles.cardTitle}>
+                            <h2 className={styles.titleText}>{title}</h2>
+                            <img
+                                className={styles.cardIcons}
+                                src="\Icons\questCompleteWarning.svg"
+                                alt="Save for later"
+                            />
+                        </div>
+                        <div className={styles.cardTags}>
+                            {tags.map((tag, index) => (
+                                <QuestTags key={index} tagName={tag} />
+                            ))}
+                        </div>
+                        <div className={styles.cardDetails}>
+                            <div className={styles.questDate}>
+                                <img className={styles.cardIcons} src="/Icons/questDate.svg" />
+                                {date}
+                            </div>
+                            <div className={styles.memberCount}>
+                                <img className={styles.cardIcons} src="/Icons/questMembers.svg" />
+                                {memberCount}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+            <div className={styles.footerBody}>
+                <div className={styles.questFeedback}>
+                    <Button
+                        text="Give Feedback"
+                        variant="noShadow"
+                        onClick={onFeedbackClick} // ✅ call the handler passed from page
+                    />
+                </div>
+            </div>
+        </CardVisual>
+    );
 }
